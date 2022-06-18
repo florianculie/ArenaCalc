f8dda0f3e79f3d1bbba1c5222b63e5578c15d58f
Florian Culié <florian.culie@bassetti.fr>
2022-06-18 23:03:46 +0200

    First working iteration
    Next up Design
    Checking if Pages works without publishing the angular sources.

--- arenacalc/src/app/bracket-calc/bracket-calc.component.ts
+++ arenacalc/src/app/bracket-calc/bracket-calc.component.ts
@@ -1,38 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-bracket-calc',
   templateUrl: './bracket-calc.component.html',
-  styleUrls: ['./bracket-calc.component.scss']
+  styleUrls: ['./bracket-calc.component.css']
 })
 export class BracketCalcComponent implements OnInit {
 
-  bracketName!: string;
-  bracketType: number = 2
+  @Input() bracketType!: number;
   ratingInput: number = 0;
-  tlkPoints!: number;
   tbcPoints!: number;
+  tlkPoints!: number;
 
 
   constructor() { }
 
   ngOnInit(): void {
-    this.computeBracket();
     this.ratingInput = 0;
-    this.tlkPoints = 0;
     this.tbcPoints = 0;
+    this.tlkPoints = 0;
   }
 
   onClick(){
-    alert(this.ratingInput)
-    this.tlkPoints = this.computeTLK(this.ratingInput);
     this.tbcPoints = this.computeTBC(this.ratingInput);
+    this.tlkPoints = this.computeTLK(this.ratingInput);
   }
 
-  computeTLK(rating : number): number{
-    return rating + 10;
-  }
-//Math.pow(number, exponent)
   computeTBC(rating : number): number{
     let exponent : number = (-0.00412 * rating);
     let number : number = 2.71828;
@@ -52,15 +45,7 @@ export class BracketCalcComponent implements OnInit {
     return Math.round(points);
   }
 
-  computeBracket(){
-    if(this.bracketType == 2){
-        this.bracketName = "2v2";
-    }
-    if(this.bracketType == 2){
-        this.bracketName = "2v2";
-    }
-    if(this.bracketType == 2){
-        this.bracketName = "2v2";
-    }
+  computeTLK(rating : number): number{
+    return rating + 10;
   }
 }
